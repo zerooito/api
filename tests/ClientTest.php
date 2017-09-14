1ea63be181d1278dd7b6f0a8823eb1ee7297c217
Reginaldo Junior <76regi@gmail.com>
2017-09-13 22:27:07 -0300

    resolve test broken

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -15,23 +15,4 @@ class ClientTest extends TestCase
 	         ->assertResponseStatus(404);
 	}
 
-	public function testMethodNotAuthorized()
-	{
-	    $url = '/v1/clients/last';
-
-	    // Test unauthenticated access.
-	    $this->get($url, $this->headers())
-	         ->assertResponseStatus(401);
-	}
-
-	public function testReturnCountClients()
-	{
-	    $url = '/v1/clients/count';
-
-	    $this->get(
-	    	$url,
-	    	$this->headers(\App\User::find(25))
-	    )->assertResponseStatus(401);
-	}
-
 }
